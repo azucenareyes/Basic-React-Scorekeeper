790b0e14123a9ce4bfcce605359193c93ff2c1f5
Azucena Martinez <azucena_rys@yahoo.com>
2018-05-17 17:47:28 -0600

    added static stylings

--- src/App.js
+++ src/App.js
@@ -13,6 +13,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="App-mainbox">
+      <h1 className="App-h1">Scoreboard</h1>
+        <div className="players">
+          <div className="first">Azucena
+            <div className="counter">
+            <div className="minus">minus
+            </div>
+            <button type="button" class="btn"> my button</button>
+            <div className="plus">plus</div>
+            </div>
+        </div>
+          <div className="second">Sofia
+            <div className="counter">
+            <div className="minus">minus
+            </div>
+            <button type="button" class="btn"> my button</button>
+            <div className="plus">plus</div>
+            </div>
+          </div>
+    </div>
+        </div>
       </div>
     );
   }
